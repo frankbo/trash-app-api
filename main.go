7317fc40536b69b741750a36a4b039bdd852d0ad
frankbo <frank.boeddeker@gmx.net>
2022-01-23 21:44:33 +0100

    Use paramater flag to chosse between local or lambda runtime

--- main.go
+++ main.go
@@ -1,21 +1,30 @@
 package main
 
 import (
+	"flag"
+	"log"
+	"net/http"
+
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/frankbo/trash-app-api/internal/handlers"
 	service "github.com/frankbo/trash-app-api/internal/services"
 )
 
 func main() {
-	lambda.Start(handlers.HandleLambdaRequest(service.FetchEvents))
+	localFlag := flag.Bool("local", false, "Run service local or as lambda")
+	flag.Parse()
 
-	// TODO with params distinguish between lambda and service.
-	// address := ":8080"
-	// http.Handle("/events", handlers.HandleHttpRequest(service.FetchEvents))
+	if *localFlag {
+		address := ":8080"
+		http.Handle("/events", handlers.HandleHttpRequest(service.FetchEvents))
 
-	// s := &http.Server{
-	// 	Addr: address,
-	// }
-	// log.Printf("Listen on %s...", address)
-	// log.Fatal(s.ListenAndServe())
+		s := &http.Server{
+			Addr: address,
+		}
+		log.Printf("Listen on %s...", address)
+		log.Fatal(s.ListenAndServe())
+	} else {
+		lambda.Start(handlers.HandleLambdaRequest(service.FetchEvents))
+		log.Printf("Run as Lambda")
+	}
 }
